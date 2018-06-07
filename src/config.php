6d9ca4ca56c8f54598b9fa06810ba2d1c84bdaab
耐小心 <qiqizjl@qq.com>
2018-06-07 15:23:25 +0800

    修复config错误

--- src/config.php
+++ src/config.php
@@ -28,7 +28,7 @@ return [
          */
         'log'           => [
             'level' => env('WECHAT_LOG_LEVEL', 'debug'),
-            'file'  => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
+            'file' => env('WECHAT_LOG_FILE', app()->getRuntimePath()."logs/wechat.log"),
         ],
     ],
 
